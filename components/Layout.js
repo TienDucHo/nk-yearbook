cdd4077a393088460eee1f679034fdf084499e70
Duc Ho <hotienduc05012002@gmail.com>
2021-10-30 05:56:17 -0600

    Changing background Image
    Using Next.js Image

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,6 @@
+// Next
+import Image from "next/image";
+
 // Components
 import Navigation from "./Navigation";
 
@@ -7,11 +10,18 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-      <div className={layoutStyles.container}>
-        <Navigation />
-        {children}
-        <Footer />
-      </div>
+    <div className={layoutStyles.container}>
+      <Image
+        className={layoutStyles.backgroundImg}
+        alt="Background"
+        src="/images/background.png"
+        layout="fill"
+        objectFit="cover"
+      />
+      <Navigation />
+      {children}
+      <Footer />
+    </div>
   );
 };
 
