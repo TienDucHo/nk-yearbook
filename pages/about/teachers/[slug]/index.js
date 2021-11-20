b3898c77edf1ba78625f9ef66ee39f3bedb55bbe
Duc Ho <hotienduc05012002@gmail.com>
2021-11-20 00:47:15 -0700

    Form fixing

--- pages/about/teachers/[slug]/index.js
+++ pages/about/teachers/[slug]/index.js
@@ -4,41 +4,80 @@ import Image from "next/image";
 
 import Confession from "@components/Confession";
 
+import TextareaAutosize from "react-textarea-autosize";
+
+import Select from "react-select";
+
 import teacherStyle from "@styles/pages/Teacher.module.scss";
 
-import { useState } from "react";
+import React, { useState } from "react";
 
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 
-const years = [];
+const yearOptions = [];
 for (var i = 1993; i <= 2021; i++) {
-  years.push(i);
+  let yearStart = (i % 100).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  let yearEnd = ((i + 3) % 100).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  yearOptions.push({
+    value: i,
+    label: `${yearStart}${yearEnd}`,
+  });
 }
+
+const customDropdownStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: "1px solid  #333333",
+    color: state.isSelected ? "white" : "#566de7",
+    padding: 20,
+  }),
+  container: (base, state) => ({
+    ...base,
+    flex: 1,
+  }),
+  control: (base, state) => ({
+    ...base,
+    background: "rgba(0, 0, 0, 0)",
+  }),
+  // control: () => ({
+  //   // none of react-select's styles are passed to <Control />
+  //   width: 200,
+  // }),
+  // singleValue: (provided, state) => {
+  //   const opacity = state.isDisabled ? 0.5 : 1;
+  //   const transition = "opacity 300ms";
+  //   return { ...provided, opacity, transition };
+  // },
+};
 
 const classes = require("libs/classes.json");
 
 const Teacher = ({ teacher }) => {
   const posts = teacher[0]["posts"];
   const [postList, updatePostList] = useState(posts);
-  const { register, handleSubmit } = useForm();
-  const onSubmit = async (data) => {
+  const { handleSubmit, control, reset } = useForm();
+  const onSubmit = (data) => {
     console.log(data);
-    let post = data;
-    post.teacher = teacher[0];
-    let newList = [...postList, post];
-    post["year"] = parseInt(post["year"]);
-    console.log(post);
-    updatePostList(newList);
-    const add = await fetch(`${server}/posts`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(post),
-    });
-    const addResponse = await add.json();
-    console.log(addResponse);
+    // post.teacher = teacher[0];
+    // let newList = [...postList, post];
+    // post["year"] = parseInt(post["year"]);
+    // updatePostList(newList);
+    // const add = await fetch(`${server}/posts`, {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(post),
+    // });
+    // const addResponse = await add.json();
+    // console.log(addResponse);
   };
   return (
     <div className={teacherStyle.container}>
@@ -59,15 +98,70 @@ const Teacher = ({ teacher }) => {
             className={teacherStyle.form}
             onSubmit={handleSubmit(onSubmit)}
           >
-            <textarea
+            <Controller
+              name="content"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextareaAutosize
+                  placeholder="LỜI YÊU THƯƠNG"
+                  {...field}
+                />
+              )}
+            />
+            <Controller
+              name="author"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextareaAutosize
+                  placeholder="NGƯỜI GỬI"
+                  {...field}
+                />
+              )}
+            />
+            <div className={teacherStyle.dropdown}>
+              <Controller
+                name="class"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={classes}
+                    styles={customDropdownStyles}
+                    placeholder="LỚP"
+                  />
+                )}
+              />
+              <Controller
+                name="year"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={yearOptions}
+                    styles={customDropdownStyles}
+                    placeholder="KHÓA"
+                  />
+                )}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Gửi"
+              className={teacherStyle.button}
+            />
+            {/* <textarea
               {...register("content", { required: true })}
               placeholder="LỜI YÊU THƯƠNG"
               type="text"
               className={teacherStyle.confessionContainer}
               cols={5}
               rows={4}
-            />
-            <input
+            /> */}
+            {/* <input
               {...register("author", { required: true })}
               placeholder="NGƯỜI GỬI"
               type="text"
@@ -79,21 +173,8 @@ const Teacher = ({ teacher }) => {
                 </option>
               ))}
             </select>
-            <select {...register("year")}>
-              {years.map((item, index) => (
-                <option value={item} key={index}>
-                  {(item % 100).toLocaleString("en-US", {
-                    minimumIntegerDigits: 2,
-                    useGrouping: false,
-                  })}
-                  {((item + 3) % 100).toLocaleString("en-US", {
-                    minimumIntegerDigits: 2,
-                    useGrouping: false,
-                  })}
-                </option>
-              ))}
-            </select>
-            <input type="submit" value="Gửi" />
+            <Select options={yearOptions} {...register("year")} />
+            <input type="submit" value="Gửi" /> */}
           </form>
         </div>
         <div className={teacherStyle.confessions}>
